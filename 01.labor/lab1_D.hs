bce5c1917a780d24bb10c8efbd5c53dba2d15eb0
BuksaAron <aronbuksa12@gmail.com>
2026-02-24 14:12:24 +0200

    labor2

--- 01.labor/lab1_D.hs
+++ 01.labor/lab1_D.hs
@@ -1,3 +1,5 @@
+import Control.Monad.RWS (MonadState(put))
+import Language.Haskell.TH (prim)
 osszeg :: Num a => a -> a -> a
 osszeg a b = a + b
 
@@ -93,3 +95,68 @@ fakt3 res n
     | n < 0 = -1
     | n == 0 = res
     | otherwise = fakt3 (res * n) (n - 1)
+
+negyzetgyokN n = [sqrt i | i <- [1 .. n]]
+
+negyzetN n = [i * i | i <- [1 .. n]]
+
+kobN n = [i ^ 3 | i <- [1 .. n]]
+
+nemnegyzetN n = [i | i <- [1 .. n], (sqrt i * sqrt i) /= i ]
+
+xhatvanyN x n = [x ^ i | i <- [1 .. n]]
+
+osztoN n = [i | i <- [1 .. n], n `mod` i == 0]
+
+osztoN2 n = [i | i <- [2, 4 .. n], mod n i == 0]
+
+osztok n = [i | i <- [1 .. n], n `mod` i == 0]
+
+primszam n = osztok n == [1, n]
+
+primszamokN n = [i | i <- [2 .. n], primszam i]
+
+primszamokN2 n = [i | i <- [1 .. n], primszamL i]
+    where
+        primszamL n = osztokL n == [1, n]
+        osztokL n = [i | i <- [1 .. n], n `mod` i == 0]
+
+osszetettN n = [i | i <- [0 .. n], not (primszam i)]
+
+paratlanosszetettN n = [i | i <- [0 .. n], not (primszam i), mod i 2 /= 0]
+
+paratlanosszetettN2 n = [i | i <- [1, 3 .. n], not (primszam i)]
+
+pitagorasz n = [(a, b, c) | c <- [1 .. n], b <- [1 .. c], a <- [1 .. b], a ^ 2 + b ^ 2 == c ^ 2]
+
+betuSzam = zip ['a' .. 'z'] [0 .. 25]
+
+szamok1 = zip[0 .. 5][5, 4 .. 0]
+
+szamok2 n = [(i, n - i) | i <- [0 .. n]]
+
+tfLs n = take n ls
+    where 
+        ls = [True, False] ++ ls
+
+main :: IO ()
+main = do
+    putStrLn("x hatvany n")
+    print(xhatvanyN 5 5)
+    putStrLn("oszto n")
+    print(osztoN 48)
+    putStrLn("oszto n 2")
+    print(osztoN2 48)
+    putStrLn("primszamok n")
+    print(primszamokN 30)
+    putStrLn("primszamok n")
+    print(primszamokN2 30)
+    putStrLn("osszetett n")
+    print(osszetettN 30)
+    putStrLn("paratlan osszetett n")
+    print(paratlanosszetettN 30)
+    putStrLn("paratlan osszetett n 2")
+    print(paratlanosszetettN2 30)
+    putStrLn("pitagorasz")
+    print(pitagorasz 100)
+    putStrLn("5 true false: " ++ show(tfLs 5))
